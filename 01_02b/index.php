6781de1e4af521aac1f4727c1e2eca62ad2441fa
valeriocicciotti <107540545+valeriocicciotti@users.noreply.github.com>
2023-01-17 11:20:11 +0000

    mod

--- 01_02b/index.php
+++ 01_02b/index.php
@@ -1,11 +1,16 @@
 <?php
 // Use the Console / Terminal for this exercise! 
-$number = 27;
-$guess = readline( 'Guess between 1 and 30: ');
-if ( $number == $guess ) {
-  echo "Yes! You guessed correctly. \n\n";
-} else {
-  echo "Alas. Your guess is incorrect. \n\n";
+$number = rand(0, 30);
+$guess = null;
+
+while ($guess != $number) {
+  $guess = readline('Guess between 1 and 30: ');
+
+  if ($number == $guess) {
+    echo "Yes! You guessed correctly. \n\n";
+  } else {
+    echo "Alas. Your guess is incorrect. \n\n";
+  }
 }
 
 die();
