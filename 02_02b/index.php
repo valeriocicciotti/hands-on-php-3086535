262b304b9f4d285a09fbcedab724cbf198e77915
valeriocicciotti <107540545+valeriocicciotti@users.noreply.github.com>
2023-01-17 16:57:03 +0000

    mod

--- 02_02b/index.php
+++ 02_02b/index.php
@@ -14,12 +14,12 @@ $extensions = array();
 foreach ($files as $file) {
 	$ext = pathinfo($file, PATHINFO_EXTENSION);
 	if (! empty($ext)) {
-		// $extensions[$ext] = ($extensions[$ext] ? ($extensions[$ext] + 1) : 1);
-		if(!array_key_exists($ext, $extensions)){
-			$extensions[$ext] = 1;
-		} else{
-			$extensions[$ext] = $extensions[$ext] + 1;
-		}
+		$extensions[$ext] = (array_key_exists($ext, $extensions) ? ($extensions[$ext] + 1) : 1);
+		// if(!array_key_exists($ext, $extensions)){
+		// 	$extensions[$ext] = 1;
+		// } else{
+		// 	$extensions[$ext] = $extensions[$ext] + 1;
+		// }
 	}
 }
 
