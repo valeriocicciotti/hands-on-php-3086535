1128f8b29cafa2302061cb5b4d63dea87a313341
valeriocicciotti <107540545+valeriocicciotti@users.noreply.github.com>
2023-01-17 10:19:57 +0000

    es. svolto

--- 01_01b/index.php
+++ 01_01b/index.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Validate Form</title>
-		<meta name="author" value="Joe Casabona" />
-	</head>
-	<body>
-		<main>
-			<?php 
-				if ( isset( $_POST['submit'] ) ) {
-					echo '<h3>Number submitted!</h3>';
-				}
-			?>
-			<form name="submit_number" method="POST">
-				<div>
-					<label for="name">Submit a Positive Integer:</label>
-					<input type="text" name="number" />
-				</div>
-				<div><input type="submit" name="submit" value="Submit Number" /></div>
-			</form>	
-		</main>
-	</body>
+
+<head>
+	<title>Validate Form</title>
+	<meta name="author" value="Joe Casabona" />
+</head>
+
+<body>
+	<main>
+		<?php
+		if (isset($_POST['submit'])) {
+			$num = (int)$_POST['number'];
+			if (is_int($num) && $num > 0) {
+				echo '<h3>Great! You have submitted a positive integer</h3>';
+			} else {
+				echo '<h3>ERROR! You did not submit a positive integer</h3>';
+			}
+		}
+		?>
+		<form name="submit_number" method="POST">
+			<div>
+				<label for="name">Submit a Positive Integer:</label>
+				<input type="text" name="number" />
+			</div>
+			<div><input type="submit" name="submit" value="Submit Number" /></div>
+		</form>
+	</main>
+</body>
+
 </html>
