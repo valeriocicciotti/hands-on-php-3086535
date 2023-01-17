8c5379ea48abf17faaf606f7bf75945fc3b2aba0
valeriocicciotti <107540545+valeriocicciotti@users.noreply.github.com>
2023-01-17 13:29:12 +0000

    mod

--- 02_01b/index.php
+++ 02_01b/index.php
@@ -1,2 +1,14 @@
 <?php
 echo 'Create your list of choices: ' . "\n\n";
+
+$choices = array();
+
+do {
+  $choices[] = readline('Add a choice? (Type 0 to stop)');
+
+} while ('0' != end($choices));
+
+array_pop($choices);
+
+echo $choices;
+
