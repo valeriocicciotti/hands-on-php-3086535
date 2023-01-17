6a066d5da61d5a62b3eac8a506b8c1aa031e13f0
valeriocicciotti <107540545+valeriocicciotti@users.noreply.github.com>
2023-01-17 15:24:59 +0000

    mod

--- 02_02b/index.php
+++ 02_02b/index.php
@@ -7,3 +7,16 @@ function print_array($a) {
 }
 
 $dir = 'files';
+$files = scandir($dir);
+
+$extensions = array();
+
+foreach ($files as $file) {
+	$ext = pathinfo($file, PATHINFO_EXTENSION);
+	if (! empty($ext)) {
+		$extensions[$ext] = ($extensions[$ext] ?? 0) + 1;
+	}
+}
+
+print_array($extensions);
+
